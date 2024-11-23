9451796f6ab0158c3f4762e190737523460ca638
ashdq <ashdiqqi3.com>
2024-11-23 18:09:04 +0700

    apple

---
+++ app/Filament/Admin/Resources/AppleResource/CreateApple.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Admin\Resources\AppleResource\Pages;
+
+use App\Filament\Admin\Resources\AppleResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateApple extends CreateRecord
+{
+    protected static string $resource = AppleResource::class;
+}
