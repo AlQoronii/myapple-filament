1fe3511e768e6fc1d972f09431ae9948a567a43f
alqoroni <fathurrhazaq@gmail.com>
2024-12-12 01:26:20 +0700

    fix(migration & seeder): categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,30 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Memasukkan data langsung tanpa menggunakan factory
-        DB::table('users')->insert([
-            [
-                'name' => 'Admin User',
-                'email' => 'admin@admin.com',
-                'password' => Hash::make('password123'),
-                'role' => 'admin', // Pastikan kolom ini ada
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'Regular User',
-                'email' => 'user@user.com',
-                'password' => Hash::make('password123'),
-                'role' => 'user', // Kolom role
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-        ]);
-
-        // Memanggil seeder lain
         $this->call([
             CategoriesSeeder::class,
             ArticleSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
